35eaec485340f24e4b6154044436c48c30c0b271
Alexandre Gigliotti <alex@panosoft.com>
2015-08-26 16:20:16 -0700

    Added tests.

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -2,12 +2,6 @@ var inline = require('inline-html');
 var path = require('path');
 var co = require('co');
 
-var definition = co.wrap(function * () {
-	return {
-		main: yield inline(path.resolve(__dirname, './index.html'))
-	};
+module.exports = co.wrap(function * () {
+	return yield inline(path.resolve(__dirname, './index.html'));
 });
-
-definition()
-	.then(console.log)
-	.catch(console.error);
--- test/index.js
+++ test/index.js
@@ -1,16 +1,32 @@
-var inlinify = require('../lib');
+var Browserify = require('browserify');
+var expect = require('chai')
+	.use(require('chai-as-promised'))
+	.expect;
+var htmlInlinify = require('../lib');
 var fs = require('fs');
 var path = require('path');
-var Browserify = require('browserify');
+var requireString = require('@panosoft/require-string');
 
-var browserify = Browserify()
-	.on('file', console.log)
-	.transform(inlinify)
-	.add('./fixtures/index.js')
-	.bundle(function (error, buffer) {
-		if (error) return console.error(error);
-		var script = buffer.toString();
-		console.log(script);
-		console.log('########################\n');
-		eval(script);
+var bundle = function (filename) {
+	return new Promise(function (resolve, reject) {
+		Browserify({standalone: 'test'})
+			.transform(htmlInlinify)
+			.add(filename)
+			.bundle(function (error, buffer) {
+				if (error) return reject(error);
+				resolve(buffer.toString());
+			});
 	});
+};
+
+describe('htmlInlinify', function () {
+	it('statically evaluate inline-html calls', function () {
+		var entry = path.resolve(__dirname, 'fixtures/index.js');
+		var html = fs.readFileSync(path.resolve(__dirname, 'fixtures/index.html'), 'utf8');
+		return bundle(entry)
+			.then(function (bundle) {
+				var exports = requireString(bundle);
+				return expect(exports()).to.eventually.equal(html);
+			});
+	});
+});
